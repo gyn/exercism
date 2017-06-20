c8d5da15f22c374b1e57b23c5ca8f5e395f559b0
Gao Ya'nan <abutter.gao@gmail.com>
2017-06-20 22:50:21 +0800

    rustfmt-ed source code
    Now it looks better

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -1,27 +1,22 @@
 pub fn score(string: &str) -> usize {
-    let mut score: usize = 0;
-
-    for c in string.chars() {
-        match c {
-            'A' | 'E' | 'I' | 'O' | 'U' | 'L' => score += 1,
-            'a' | 'e' | 'i' | 'o' | 'u' | 'l' => score += 1,
-            'N' | 'R' | 'S' | 'T' => score += 1,
-            'n' | 'r' | 's' | 't' => score += 1,
-            'D' | 'G' => score += 2,
-            'd' | 'g' => score += 2,
-            'B' | 'C' | 'M' | 'P' => score += 3,
-            'b' | 'c' | 'm' | 'p' => score += 3,
-            'F' | 'H' | 'V' | 'W' | 'Y' => score += 4,
-            'f' | 'h' | 'v' | 'w' | 'y' => score += 4,
-            'K' => score += 5,
-            'k' => score += 5,
-            'J' | 'X' => score += 8,
-            'j' | 'x' => score += 8,
-            'Q' | 'Z' => score += 10,
-            'q' | 'z' => score += 10,
-            _ => score += 0,
-        }
-    }
-
-    score
+    string
+        .chars()
+        .map(|c| match c {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+            'D' | 'G' => 2,
+            'd' | 'g' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'b' | 'c' | 'm' | 'p' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'f' | 'h' | 'v' | 'w' | 'y' => 4,
+            'K' => 5,
+            'k' => 5,
+            'J' | 'X' => 8,
+            'j' | 'x' => 8,
+            'Q' | 'Z' => 10,
+            'q' | 'z' => 10,
+            _ => 0,
+        })
+        .sum()
 }
