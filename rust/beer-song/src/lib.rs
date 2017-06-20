4b3d921164f8c521d1ec05cd2ed7958d7c1ece45
yanan.gao <yanan.gao@imgtec.com>
2017-06-20 11:10:20 +0800

    Improve the code
    The functional programming looks clean and cool

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,55 +1,40 @@
 pub fn verse(index: usize) -> String {
-    let mut first = String::new();
-    let mut second = String::new();
-    let third: &str;
-    let mut last = String::new();
-
     match index {
         2 => {
-            first.push_str("2 bottles");
-            second = first.clone();
-            third = "Take one down and pass it around, ";
-            last.push_str("1 bottle of");
+            "2 bottles of beer on the wall, 2 bottles of beer.\n\
+           Take one down and pass it around, 1 bottle of beer \
+           on the wall.\n"
+                .to_string()
         }
         1 => {
-            first.push_str("1 bottle");
-            second = first.clone();
-            third = "Take it down and pass it around, ";
-            last.push_str("no more bottles of");
+            "1 bottle of beer on the wall, 1 bottle of beer.\n\
+           Take it down and pass it around, no more bottles of beer \
+           on the wall.\n"
+                .to_string()
         }
         0 => {
-            first.push_str("No more bottles");
-            second.push_str("no more bottles");
-            third = "Go to the store and buy some more, ";
-            last.push_str("99 bottles of");
+            "No more bottles of beer on the wall, no more bottles of beer.\n\
+            Go to the store and buy some more, 99 bottles of beer \
+            on the wall.\n"
+                .to_string()
         }
         _ => {
-            first = format!("{} bottles", index);
-            second = first.clone();
-            third = "Take one down and pass it around, ";
-            last = format!("{} bottles of", index - 1);
+            format!(
+                "{} bottles of beer on the wall, {} bottles of beer.\n\
+                Take one down and pass it around, {} bottles of beer \
+                on the wall.\n",
+                index,
+                index,
+                index - 1
+            )
         }
-    };
-
-    format!(
-        "{} of beer on the wall, {} of beer.\n{}{} beer on the wall.\n",
-        first,
-        second,
-        third,
-        last
-    )
+    }
 }
 
 pub fn sing(upper: usize, lower: usize) -> String {
-    let mut result = String::new();
-
-    for i in (lower..upper + 1).rev() {
-        result = result + &verse(i);
-
-        result.push('\n');
-    }
-
-    result.pop();
-
-    result
+    (lower..upper + 1)
+        .rev()
+        .map(verse)
+        .collect::<Vec<_>>()
+        .join("\n")
 }
