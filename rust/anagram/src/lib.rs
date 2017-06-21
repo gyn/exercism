918aca24e74f6067efb9408a31eca7f5428e7c78
Gao Ya'nan <abutter.gao@gmail.com>
2017-06-21 21:16:41 +0800

    Add rust code for anagram case
    It is ugly now but works

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+fn anagrams(word: &str) -> HashMap<char, u32> {
+    let mut result: HashMap<char, u32> = HashMap::new();
+
+    for k in word.chars() {
+        let v = match result.get(&k) {
+            Some(v) => v + 1,
+            None => 1,
+        };
+
+        result.insert(k, v);
+    }
+
+    result
+}
+
+pub fn anagrams_for(word: &str, input: &[&'static str]) -> Vec<&'static str> {
+    let lword = word.to_lowercase();
+
+    let dict = anagrams(&lword);
+
+    let mut result: Vec<&str> = Vec::new();
+
+    for s in input {
+        let ls = s.to_lowercase();
+
+        if ls == lword {
+            continue;
+        }
+
+        if anagrams(&ls) == dict {
+            result.push(s);
+        }
+    }
+
+    result
+}
