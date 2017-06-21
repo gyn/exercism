ac8b8159f8da6a9776ee58cae0a12db953a69aca
yanan.gao <yanan.gao@imgtec.com>
2017-06-21 15:46:15 +0800

    Change rust code for etl
    I like this functional programming style

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -2,13 +2,8 @@ use std::collections::BTreeMap;
 use std::ascii::AsciiExt;
 
 pub fn transform(input: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut result: BTreeMap<char, i32> = BTreeMap::new();
-
-    for (k, v) in input {
-        for c in v {
-            result.entry((*c).to_ascii_lowercase()).or_insert(*k);
-        }
-    }
-
-    result
+    input
+        .iter()
+        .flat_map(|(k, v)| v.iter().map(move |x| (x.to_ascii_lowercase(), *k)))
+        .collect()
 }
