987e3192733ee84f02aa04c39a0bbf17a03ce0aa
Gao Ya'nan <abutter.gao@gmail.com>
2017-07-06 18:42:32 +0800

    Replace u8 with bool
    So the code becomes shorter and clean

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -2,15 +2,15 @@
 use std::ascii::AsciiExt;
 
 pub fn is_pangram(words: &str) -> bool {
-    let score = &mut [0u8; 26];
+    let score = &mut [false; 26];
 
     for v in words.chars() {
         if !v.is_ascii_alphabetic() {
             continue;
         }
 
-        score[v.to_ascii_lowercase() as usize - 'a' as usize] = 1;
+        score[v.to_ascii_lowercase() as usize - 'a' as usize] = true;
     }
 
-    score.iter().all(|&x| x == 1u8)
+    score.iter().all(|&x| x)
 }
