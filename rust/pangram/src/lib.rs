e024d0c71c2226f7349546311c38cbef80465ad4
Gao Ya'nan <abutter.gao@gmail.com>
2017-07-07 19:52:33 +0800

    Clean up rust code for pangram
    It looks better and should be more efficent than the contains ones

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -5,13 +5,11 @@ pub fn is_pangram(words: &str) -> bool {
     let score = &mut [false; 26];
 
     for v in words.chars() {
-        if !v.is_ascii_alphabetic() {
-            continue;
+        if v.is_ascii_alphabetic() {
+            let index = v.to_ascii_lowercase() as usize - 'a' as usize;
+
+            score[index] = true;
         }
-
-        let index = v.to_ascii_lowercase() as usize - 'a' as usize;
-
-        score[index] = true;
     }
 
     score.iter().all(|&x| x)
