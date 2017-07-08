dc549d85b2475b4b43ecdad2b0c3ec5e0d87ba58
Gao Ya'nan <abutter.gao@gmail.com>
2017-07-08 18:08:35 +0800

    Add rust code for space-age
    The associated consts is a good feature

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -8,8 +8,10 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
+    const RATIO: f64;
+
     fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / 31557600.0
+        d.0 as f64 / 31557600.0 / Self::RATIO
     }
 }
 
@@ -23,49 +25,33 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (0.2408467 * 31557600.0)
-    }
+    const RATIO: f64 = 0.2408467;
 }
 
 impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (0.61519726 * 31557600.0)
-    }
+    const RATIO: f64 = 0.61519726;
 }
 
 impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (1.0 * 31557600.0)
-    }
+    const RATIO: f64 = 1.0;
 }
 
 impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (1.8808158 * 31557600.0)
-    }
+    const RATIO: f64 = 1.8808158;
 }
 
 impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (11.862615 * 31557600.0)
-    }
+    const RATIO: f64 = 11.862615;
 }
 
 impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (29.447498 * 31557600.0)
-    }
+    const RATIO: f64 = 29.447498;
 }
 
 impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (84.016846 * 31557600.0)
-    }
+    const RATIO: f64 = 84.016846;
 }
 
 impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / (164.79132 * 31557600.0)
-    }
+    const RATIO: f64 = 164.79132;
 }
