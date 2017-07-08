d5e170ad7496b7eb157b89e817ec1cdc8432f7ac
Gao Ya'nan <abutter.gao@gmail.com>
2017-07-08 10:51:04 +0800

    Add rust code for rna-transcription
    It passes all tests

---
+++ rust/rna-transcription/src/lib.rs
@@ -0,0 +1,36 @@
+#[derive(Debug, PartialEq)]
+pub struct RibonucleicAcid(String);
+
+impl RibonucleicAcid {
+    pub fn new(serial: &str) -> RibonucleicAcid {
+        RibonucleicAcid(serial.to_string())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct DeoxyribonucleicAcid(String);
+
+impl DeoxyribonucleicAcid {
+    pub fn new(serial: &str) -> DeoxyribonucleicAcid {
+        DeoxyribonucleicAcid(serial.to_string())
+    }
+
+    pub fn to_rna(&self) -> Result<RibonucleicAcid, String> {
+        let rna = self.0
+            .chars()
+            .filter_map(|c| match c {
+                'A' => Some('U'),
+                'C' => Some('G'),
+                'G' => Some('C'),
+                'T' => Some('A'),
+                _ => None,
+            })
+            .collect::<String>();
+
+        if rna.len() != self.0.len() {
+            return Err("Invalid DNA serial".to_string());
+        }
+
+        Ok(RibonucleicAcid(rna))
+    }
+}
