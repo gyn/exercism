1857c88ff7127689da538b31cdce7a9839cda61c
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-11 04:33:19 +0800

    Use const instead of magic number
    It looks better

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -2,14 +2,16 @@
 use std::ascii::AsciiExt;
 
 pub fn rotate(text: &str, key: usize) -> String {
+    const LA: u8 = b'a';
+    const UA: u8 = b'A';
     text.chars()
         .map(|x| match x {
             _ if x.is_ascii_lowercase() => {
-                let v = (x as u8 - b'a' + key as u8) % 26 + b'a';
+                let v = (x as u8 - LA + key as u8) % 26 + LA;
                 v as char
             }
             _ if x.is_ascii_uppercase() => {
-                let v = (x as u8 - b'A' + key as u8) % 26 + b'A';
+                let v = (x as u8 - UA + key as u8) % 26 + UA;
                 v as char
             }
             _ => x,
