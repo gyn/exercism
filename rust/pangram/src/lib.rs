1c576775ff3958b2c71f97c63f04168071517365
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-11 04:31:23 +0800

    Use bits as counters
    It is very C style

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -2,15 +2,14 @@
 use std::ascii::AsciiExt;
 
 pub fn is_pangram(words: &str) -> bool {
-    let score = &mut [false; 26];
+    let mut score = 0u32;
 
     for v in words.chars() {
         if v.is_ascii_alphabetic() {
             let index = v.to_ascii_lowercase() as usize - 'a' as usize;
-
-            score[index] = true;
+            score |= 1 << index;
         }
     }
 
-    score.iter().all(|&x| x)
+    score == (1 << 26) - 1
 }
