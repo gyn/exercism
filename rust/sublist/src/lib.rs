36f65451264521f082fba3f59cc85c515287dbef
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-11 02:56:17 +0800

    A better one for sublist case
    The code is stolen from @chenxiaolong

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,43 +6,21 @@ pub enum Comparison {
     Superlist,
 }
 
-#[inline]
-fn contains<T>(short: &[T], longer: &[T]) -> Comparison
-where
-    T: PartialEq,
-{
-    let size = short.len();
-
-    if longer.windows(size).any(|x| x == short) {
-        return Comparison::Sublist;
-    }
-
-    Comparison::Unequal
-}
-
 pub fn sublist<T>(a: &[T], b: &[T]) -> Comparison
 where
     T: PartialEq,
 {
-    if a.is_empty() && !b.is_empty() {
-        return Comparison::Sublist;
-    }
+    let (needle, haystack, result) = if a.len() < b.len() {
+        (a, b, Comparison::Sublist)
+    } else if a.len() > b.len() {
+        (b, a, Comparison::Superlist)
+    } else {
+        (a, b, Comparison::Equal)
+    };
 
-    if !a.is_empty() && b.is_empty() {
-        return Comparison::Superlist;
+    if needle.is_empty() || haystack.windows(needle.len()).any(|w| w == needle) {
+        result
+    } else {
+        Comparison::Unequal
     }
-
-    if a.len() < b.len() {
-        return contains(a, b);
-    }
-
-    if a.len() == b.len() && a == b {
-        return Comparison::Equal;
-    }
-
-    if a.len() > b.len() && contains(b, a) == Comparison::Sublist {
-        return Comparison::Superlist;
-    }
-
-    Comparison::Unequal
 }
