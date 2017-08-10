ece4392a0a0900957b3c47412c8ca419767de136
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-11 04:36:59 +0800

    Use entry instead of get
    It looks better

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -3,16 +3,15 @@ use std::ascii::AsciiExt;
 use std::collections::HashMap;
 
 pub fn word_count(text: &str) -> HashMap<String, u32> {
+    let mut result: HashMap<String, u32> = HashMap::new();
+
     let iter = text.split(|x: char| x.is_ascii_punctuation() || x == ' ')
         .filter(|x| !x.is_empty())
         .map(|x| x.to_lowercase());
 
-    let mut result: HashMap<String, u32> = HashMap::new();
-
     for i in iter {
-        let v = result.get(&i).map_or(1, |v| v + 1);
-
-        result.insert(i, v);
+        let entry = result.entry(i).or_insert(0);
+        *entry += 1;
     }
 
     result
