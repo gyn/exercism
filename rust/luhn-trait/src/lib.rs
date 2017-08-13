f1f42cd0371c57232eeb88769565069c3b480687
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-13 21:23:28 +0800

    Add rust code for luhn-trait
    It passed all testcases

---
+++ rust/luhn-trait/src/lib.rs
@@ -0,0 +1,52 @@
+pub trait LuhnTrait {
+    fn valid_luhn(&self) -> bool;
+}
+
+impl<U> LuhnTrait for U
+where
+    U: ToString,
+{
+    fn valid_luhn(&self) -> bool {
+        is_valid(&self.to_string())
+    }
+}
+
+pub fn is_valid(id: &str) -> bool {
+    if id.len() < 2 {
+        return false;
+    }
+
+    let mut index: u32 = 0;
+    let mut sum: u32 = 0;
+
+    for c in id.chars().rev() {
+        if c.is_whitespace() {
+            continue;
+        }
+
+        let mut v: u32;
+
+        if let Some(d) = c.to_digit(10) {
+            v = d;
+        } else {
+            return false;
+        }
+
+        if index % 2 != 0 {
+            v *= 2;
+
+            if v > 9 {
+                v -= 9;
+            }
+        }
+
+        index += 1;
+        sum += v;
+    }
+
+    if index < 2 {
+        return false;
+    }
+
+    (sum % 10) == 0
+}
