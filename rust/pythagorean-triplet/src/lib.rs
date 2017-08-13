72c3995b68239e5d48aacdf5dfa503f8a8754664
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-13 19:16:46 +0800

    Use const instead of magic number
    It looks better now

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,16 +1,17 @@
 pub fn find() -> Option<u32> {
-    const LOWER: u32 = 1000/3;
-    const UPPER: u32 = 1000/2;
+    const VALUE: u32 = 1000;
+    const LOWER: u32 = VALUE / 3;
+    const UPPER: u32 = VALUE / 2;
 
     for c in LOWER..UPPER {
-        for b in c/2..c {
+        for b in c / 2..c {
             for a in 1..b {
-                if a*a + b*b != c*c {
+                if a * a + b * b != c * c {
                     continue;
                 }
 
-                if a + b + c == 1000 {
-                    return Some(a*b*c);
+                if a + b + c == VALUE {
+                    return Some(a * b * c);
                 }
             }
         }
