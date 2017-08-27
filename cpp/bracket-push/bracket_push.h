2fd7e0e67dd9d018a58690e2ced6e1a4a0775e95
Gao Ya'nan <abutter.gao@gmail.com>
2017-08-27 16:45:34 +0800

    Add c++ code for bracket_push case
    It passed all test cases

---
+++ cpp/bracket-push/bracket_push.h
@@ -0,0 +1,61 @@
+#ifndef BRACKET_PUSH_H
+#define BRACKET_PUSH_H
+
+#include <string>
+#include <stack>
+#include <iostream>
+
+namespace bracket_push {
+    static inline bool is_matched(std::stack<char> &v, char expected) {
+        if (v.empty() || v.top() != expected) {
+            return false;
+        }
+
+        v.pop();
+
+        return true;
+    }
+
+    static inline bool check(std::string input) {
+        char expected;
+        std::stack<char> v;
+
+        for (auto &c : input) {
+            switch (c) {
+                case '[':
+                case '{':
+                case '(':
+                    v.push(c);
+                    break;
+
+                case ']':
+                    expected = '[';
+                    if (!is_matched(v, expected)) {
+                        return false;
+                    }
+                    break;
+
+                case '}':
+                    expected = '{';
+                    if (!is_matched(v, expected)) {
+                        return false;
+                    }
+                    break;
+
+                case ')':
+                    expected = '(';
+                    if (!is_matched(v, expected)) {
+                        return false;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return v.empty();
+    }
+}
+
+#endif//BRACKET_PUSH_H
