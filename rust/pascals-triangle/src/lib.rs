1eef71dab7610fceea009c81a86e10a7084cd1e0
Gao Ya'nan <abutter.gao@gmail.com>
2017-09-04 01:19:16 +0800

    Add rust code for pascals-triangle case
    It passed all testcases

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,27 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    row: u32,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!();
+        PascalsTriangle { row: row_count }
+    }
+
+    fn pascal(row: u32) -> Vec<u32> {
+        let mut result = Vec::with_capacity(row as usize);
+
+        result.push(1);
+
+        for k in 0..row {
+            let v = result[k as usize] * (row - k) / (k + 1);
+
+            result.push(v);
+        }
+
+        result
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        (0..self.row).map(PascalsTriangle::pascal).collect()
     }
 }
