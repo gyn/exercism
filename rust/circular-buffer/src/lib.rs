000b6286af347746d11498e60678ac677a99d5ee
Gao Ya'nan <abutter.gao@gmail.com>
2017-09-18 19:29:41 +0800

    Add special error code for the case size is 0
    Even though the test cases don't use size as 0

--- rust/circular-buffer/src/lib.rs
+++ rust/circular-buffer/src/lib.rs
@@ -8,6 +8,7 @@ pub struct CircularBuffer<T> {
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
+    InvalidSize,
     EmptyBuffer,
     FullBuffer,
 }
@@ -22,14 +23,18 @@ impl<T> CircularBuffer<T> {
 
     pub fn read(&mut self) -> Result<T, Error> {
         if self.size == 0 {
-            return Err(Error::FullBuffer);
+            return Err(Error::InvalidSize);
         }
 
         self.buffer.pop_front().ok_or(Error::EmptyBuffer)
     }
 
     pub fn write(&mut self, v: T) -> Result<usize, Error> {
-        if self.size == 0 || self.buffer.len() == self.size {
+        if self.size == 0 {
+            return Err(Error::InvalidSize);
+        }
+
+        if self.buffer.len() == self.size {
             return Err(Error::FullBuffer);
         }
 
