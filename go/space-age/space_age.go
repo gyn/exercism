52288c95cee9fc2a0c7836a254b9c77c53ae7888
Gao Ya'nan <abutter.gao@gmail.com>
2018-03-12 23:16:09 -0400

    Add go code for space age case
    It passed all testcase but I have some doubts on the default branch

---
+++ go/space-age/space_age.go
@@ -0,0 +1,30 @@
+package space
+
+type Planet string
+
+func Age(second float64, planet Planet) float64 {
+	const scale = 31557600.0
+
+	var factor float64
+	switch planet {
+	case "Earth":
+		factor = 1.0
+	case "Mercury":
+		factor = 0.2408467
+	case "Venus":
+		factor = 0.61519726
+	case "Mars":
+		factor = 1.8808158
+	case "Jupiter":
+		factor = 11.862615
+	case "Saturn":
+		factor = 29.447498
+	case "Uranus":
+		factor = 84.016846
+	case "Neptune":
+		factor = 164.79132
+	default:
+	}
+	
+	return second / scale / factor
+}
