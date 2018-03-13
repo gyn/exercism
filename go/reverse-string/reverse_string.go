2144bfe5f74f4224d2a187e2319b0f287725b7b8
Gao Ya'nan <abutter.gao@gmail.com>
2018-03-12 23:37:57 -0400

    Add go code for reverse string case
    I stolen the code form Russ Cox

---
+++ go/reverse-string/reverse_string.go
@@ -0,0 +1,18 @@
+package reverse
+
+// String reversed the input string
+func String(input string) string {
+	n := 0
+	rune := make([]rune, len(input))
+	for _, r := range input {
+		rune[n] = r
+		n++
+	}
+	rune = rune[0:n]
+	// Reverse
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+
+	return string(rune)
+}
